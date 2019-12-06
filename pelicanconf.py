9c6fe25e7214e4d13f08eb3bcd2147a8442bed11
mango <8082526+ashleygar@users.noreply.github.com>
2019-12-06 11:34:06 -0500

    commit

---
+++ pelicanconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Ashley Gardner'
+SITENAME = 'blog'
+SITEURL = 'https://ashleygar.github.io/blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
